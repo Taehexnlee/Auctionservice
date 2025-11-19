9b47bac7f0d836869d353fb249821a64c28d7c49
Lee Tae Heon <thhoney218@gmail.com>
2025-11-19 11:56:59 +1100

    Added Gateway service

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,29 @@
+using System;
+using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFInished>
+{
+    private readonly AuctionDbContext _dbcontext;
+    public AuctionFinishedConsumer(AuctionDbContext dbContext)
+    {
+        _dbcontext = dbContext;
+    }
+    public async Task Consume(ConsumeContext<AuctionFInished> context)
+    {   
+        Console.WriteLine("--> Consuming Auction Finished");
+        var auction = await _dbcontext.Auctions.FindAsync(context.Message.AuctionId);
+        if(context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+        auction.Status = auction.SoldAmount > auction.ReservePrice
+            ? Status.Finished : Status.ReserveNotMet;
+        await _dbcontext.SaveChangesAsync();
+    }
+}
---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,23 @@
+using System;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Model;
+
+namespace SearchService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFInished>
+{
+    public async Task Consume(ConsumeContext<AuctionFInished> context)
+    {
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);        
+
+        if(context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = (int)context.Message.Amount;
+        }
+        auction.Status= "Finished";
+        await auction.SaveAsync();
+    }
+}
