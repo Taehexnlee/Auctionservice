327f9fff404bbd76c5263175c710c45e6543dcab
Lee Tae Heon <thhoney218@gmail.com>
2025-11-21 14:50:51 +1100

    Client side identity with NextAuth

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,12 +1,21 @@
+import { getCurrentUser } from "../actions/authAction";
+import LoginButton from "./LoginButton";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserActions from "./UserActions";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getCurrentUser();
   return (
     <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-grey-800 shadow-md">
-        <Logo/>
-        <Search/>
-        <div>Login</div>
+      <Logo />
+      <Search />
+      {user ? (
+        <UserActions user={user} />
+      ) : (
+        <LoginButton />
+      )}
+
     </header>
   )
 }
